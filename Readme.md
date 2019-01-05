7ad263e243c083638b575a302da8c4a55c220146
JideGuru <guruliciousjide@gmail.com>
2019-01-05 23:08:11 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,44 @@
+## iJokeBot
+
+A Telegram Bot that send Jokes to people or group.
+
+# Requirements
+* Any Operating System (ie. MacOS X, Linux, Windows).
+* Any IDE to code Python (ie. Pycharm, Notepad++, Sublime Text, VSCode etc).
+* A little knowledge of Python.
+* A brain to think 🤓🤓
+
+# Usage
+
+Create a telegram bot and replace its token with the token in the code.
+
+```token = "7********************************"```
+
+# Author(s)
+**Olusegun Festus Babajide**
+
+
+# License
+```
+Copyright [2019] [JideGuru]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
+
+
+# Disclaimer
+This code is only intended for learning purposes, i am not responsible for anything you use it for.
+
+Pardon my Bad English 😔
+
+Give a **Star** if you like what you see.
---
+++ main.py
@@ -0,0 +1,55 @@
+import requests
+import datetime
+
+from bothandler import BotHandler
+
+token = "797527677:AAEbqc5eLLqvtG9_JUEEXrp1sXXtOE2TeFU"
+
+jokes_bot = BotHandler(token)
+greetings = ('hello', 'hi', 'greetings', 'sup')
+now = datetime.datetime.now()
+
+
+def main():
+    new_offset = None
+    today = now.day
+    hour = now.hour
+
+    while True:
+        jokes_bot.get_updates(new_offset)
+
+        last_update = jokes_bot.get_last_update()
+        get_joke = jokes_bot.get_jokes()
+
+        last_update_id = last_update['update_id']
+        last_chat_text = last_update['message']['text']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['from']['first_name']
+
+        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+            jokes_bot.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+            jokes_bot.send_message(last_chat_id, 'Good Afternoon {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+            jokes_bot.send_message(last_chat_id, 'Good Evening  {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() == "/jokes" or last_chat_text.lower() == "/jokes":
+
+            jokes_bot.send_message(last_chat_id, get_joke)
+
+        elif hour == 11:
+            jokes_bot.send_message(last_chat_id, get_joke)
+
+        new_offset = last_update_id + 1
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
